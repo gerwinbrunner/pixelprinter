d83f8e9360474f09a51376b13ea3df03535e00a7
Dennis Theisen <dennis@jadedpixel.com>
2009-04-08 15:23:42 -0400

    Removed duplicate preview link and added a new template when creating a shop.

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -9,5 +9,7 @@ class Shop < ActiveRecord::Base
   # Create 3 templates as a starting point for the user
   def create_base_templates
     templates.new.from_file(:invoice)
+    # TODO: add another default template and put it in the correct folder
+    templates.new.from_file("../../test/fixtures/example_print_template")
   end
 end
