0a86579bb1b3459d291adabc532e81b3adec9e01
Dennis Theisen <dennis@jadedpixel.com>
2009-06-02 00:17:40 -0400

    FIX: redirect to 'show' instead of 'index' of course, for this url: /orders/id=123

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ ActionController::Routing::Routes.draw do |map|
   map.root :controller => 'login'
 
   # this route is needed for Shopify's application links, because they append the id param with a question mark instead of rails nested style
-  map.connect 'orders?id=:id', :controller => 'orders', :action => 'index'
+  map.connect '/orders?id=:id', :controller => 'orders', :action => 'show'
   map.resources :orders, :only => [:index, :show], :member => {:preview => :get, :print => :post}
   map.resources :print_templates, :as => 'templates'
   
