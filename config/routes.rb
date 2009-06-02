5a9f2cb34f4dc062a0089c7bd2657a4201b19412
Dennis Theisen <dennis@jadedpixel.com>
2009-06-02 00:14:55 -0400

    Add a new route to intercept redirects from Shopify like this: /orders?id=123

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 ActionController::Routing::Routes.draw do |map|
   map.root :controller => 'login'
 
+  # this route is needed for Shopify's application links, because they append the id param with a question mark instead of rails nested style
+  map.connect 'orders?id=:id', :controller => 'orders', :action => 'index'
   map.resources :orders, :only => [:index, :show], :member => {:preview => :get, :print => :post}
   map.resources :print_templates, :as => 'templates'
   
