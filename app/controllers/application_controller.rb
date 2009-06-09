a6c5ab2aa57d61f49840df42a1ec83166b387c4f
Dennis Theisen <dennis@jadedpixel.com>
2009-06-09 11:59:36 -0400

    Integrated Hoptoad exception notification. More:   * Better error message when order or template can't be found   * Hoptoad API key is added through heroku key interface (command line tool)   * Extracted Shopify API key and secret also to heroku key interface (add via command line tool)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,5 +13,13 @@ class ApplicationController < ActionController::Base
   def shop
     Shop.find_or_create_by_url(current_shop.url)
   end
-  
+
+  # Show 404 page with better error explanation when an Order or Template can't be found
+  def rescue_action_in_public(exception)
+    if ActiveResource::ResourceNotFound || ActiveRecord::RecordNotFound
+      render :file => "#{RAILS_ROOT}/public/404.html", :status => 404
+    else
+      super
+    end
+  end
 end
--- db/migrate/20090605160611_update_content_of_unmodified_default_templates.rb
+++ db/migrate/20090605160611_update_content_of_unmodified_default_templates.rb
@@ -6,8 +6,8 @@ class UpdateContentOfUnmodifiedDefaultTemplates < ActiveRecord::Migration
                    memo
                  end
   
-  ORIGINAL_MD5 = { "invoice"            => ["3cbe75be4e5559a0643581bb48c43222", "cb44126c6c299701fef22058670ae34f"],
-                   "packing_slip"       => ["84054e0b1b3659a969c0d3b984aff8e7", "cbaba8dfbcc4b64955b7ed12c708a4ea"], 
+  ORIGINAL_MD5 = { "invoice"            => ["3cbe75be4e5559a0643581bb48c43222", "cb44126c6c299701fef22058670ae34f", "77975982a6f49bc2eebf3248f4aaddbf"],
+                   "packing_slip"       => ["84054e0b1b3659a969c0d3b984aff8e7", "cbaba8dfbcc4b64955b7ed12c708a4ea", "5546720aa939062cae7b63861663c331"], 
                    "variable_reference" => ["caa1eb749f63a296ebddf9019ba188c8", "328811bd3ee651c26f1e1c46bc8850eb", "b1775d4c8c17320f45a3d40601562f02"]
                  }
                  
